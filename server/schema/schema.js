c38d1456b5f0fa846e60df22c46fc1fcf00fee61
sn <saravanan.n@radisys.com>
2021-11-03 19:08:51 +0530

    Dummy data, lodash, resolver, graphiql

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,6 +1,14 @@
 const graphql = require('graphql');
+const _ = require('lodash');
 
 const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+
+// Dummy data
+const books = [
+  {id:'1', name: 'BOOK 1', genre: 'Sci-fi'},
+  {id:'2', name: 'BOOK 2', genre: 'Action'},
+  {id:'3', name: 'BOOK 3', genre: 'Adventure'},
+];
 
 const BookType = new GraphQLObjectType({
   name: 'Book',
@@ -18,8 +26,8 @@ const RootQuery = new GraphQLObjectType({
       type: BookType,
       args: { id:{ type: GraphQLString } },
       resolve(parent, args) {
-        console.log(args);
         // code to get data from db / other source
+        return _.find(books, {id: args.id});
       }
     }
   }
